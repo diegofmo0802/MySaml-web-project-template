eda124fd1b24345de6ca08e157a06b4d398fb059
diegofmo0802 <diegofmo0802@gmail.com>
2025-11-07 20:33:15 +0000

    fixed evaluation

--- idx-template.nix
+++ idx-template.nix
@@ -8,7 +8,8 @@
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
 
     npm install typescript @types/node
-    npm install saml.servercore${ if mode == "dev" then "@dev" else "" }
+    npm install saml.servercore${ if
+     mode == "dev" then "@dev" else "" }
 
     ./build.bash
   '';
