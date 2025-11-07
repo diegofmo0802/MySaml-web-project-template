49a24e74f7d87e76ae8e4b16395be36ee47bc176
diegofmo0802 <diegofmo0802@gmail.com>
2025-11-07 23:00:29 +0000

    added permissions to scripts

--- idx-template.nix
+++ idx-template.nix
@@ -9,6 +9,10 @@
 
     cd "$out"
 
+    chmod +x ./build.bash
+    chmod +x ./dependencies.bash
+    chmod +x ./.mongodb/start.bash
+
     npm install typescript @types/node
     npm install saml.servercore${ if mode == "dev" then "@dev" else "" }
 
