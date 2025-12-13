c9a498c9b43e8e08ba5086876442a594fe117fdb
diegofmo0802 <diegofmo0802@gmail.com>
2025-12-13 01:39:18 +0000

    refactor: fix bad copy

--- idx-template.nix
+++ idx-template.nix
@@ -7,18 +7,13 @@
   bootstrap = ''
     set -e
 
-    cp -R .idx $out
-    cp -R .vscode $out
-    cp -R template/* $out
+    cp -R ${./.}/.idx $out
+    cp -R ${./.}/.vscode $out
+    cp -R ${./.}/template/* $out
 
     cd $out
     npm init -y
-    node -e "
-      const fs = require('fs');
-      const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-      pkg.type = 'module';
-      fs.writeFileSync('package.json', JSON.stringify(pkg, null, 4));
-    "
+    npm pkg set type=module
     npm install -D typescript @types/node
     npm install saml.servercore${if mode == "dev" then "@dev" else ""}
     npm install -g saml.dep-manager
