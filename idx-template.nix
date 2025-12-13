0ba2a2442f48fec05435fb7fe23ae487477ea305
diegofmo0802 <diegofmo0802@gmail.com>
2025-12-13 22:01:55 +0000

    feat: modified idx-template.nix to modify package.json

--- idx-template.nix
+++ idx-template.nix
@@ -14,12 +14,19 @@
 
     cd "$out"
     npm init -y
-    npm pkg set type=module
+    npm pkg set type="module"
+    npm pkg set main="build/server.js"
+    npm pkg set script.compile="npx tsc && npx tsc -p client/logic"
+    npm pkg set script.build="npm install && npx dep install && cd client/logic && npx tsc && cd ../../ && npm run compile"
+    npm pkg set script.dev="npx tsc --watch"
+    npm pkg set script.dev-client="npx tsc -p client/logic --watch"
+    npm pkg set script.start="node build/server.js"
     npm install -D typescript @types/node
     npm install saml.servercore${if mode == "dev" then "@dev" else ""}
     npm install saml.dep-manager
     npx dep install
 
+    ls -la
     chmod +x .mongodb/start.bash
   '';
 }
