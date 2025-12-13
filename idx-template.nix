8294a69e989b230dd4a111a20cad23a8bde30160
diegofmo0802 <diegofmo0802@gmail.com>
2025-12-13 01:23:21 +0000

    added initialization sequence

--- idx-template.nix
+++ idx-template.nix
@@ -3,21 +3,25 @@
     pkgs.git
     pkgs.nodejs
   ];
+
   bootstrap = ''
-    cp -rf ${./.} "$out"
-    chmod -R +w "$out"
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+    set -e
 
-    cd "$out"
+    cp -R .idx $out
+    cp -R .vscode $out
+    cp -R template/* $out
 
-    chmod +x ./build.bash
-    chmod +x ./dependencies.bash
-    chmod +x ./.mongodb/start.bash
+    cd $out
+    npm init -y
+    node -e "
+      const fs = require('fs');
+      const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+      pkg.type = 'module';
+      fs.writeFileSync('package.json', JSON.stringify(pkg, null, 4));
+    "
+    npm install -D typescript @types/node
+    npm install saml.servercore${if mode == "dev" then "@dev" else ""}
+    npm install -g saml.dep-manager
 
-    npm install typescript @types/node
-    npm install mongodb
-    npm install saml.servercore${ if mode == "dev" then "@dev" else "" }
-
-    ./build.bash
   '';
 }
