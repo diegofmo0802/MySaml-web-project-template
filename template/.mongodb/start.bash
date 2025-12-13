36a0cb95c815ea407b085dd194d52d3d63c76899
diegofmo0802 <diegofmo0802@gmail.com>
2025-12-13 23:31:33 +0000

    added function to initiate replication server

--- template/.mongodb/start.bash
+++ template/.mongodb/start.bash
@@ -1,4 +1,27 @@
 #!/usr/bin/bash
-mkdir -p ./.mongodb/var/lib/mongo/
-mkdir -p ./.mongodb/var/log/mongodb/
-mongod --config ./.mongodb/mongo.conf
+
+running=$(mongo --quiet --eval "db.adminCommand('ping').ok" || echo "0");
+
+if [ "$running" != "1" ]; then
+    mkdir -p ./.mongodb/var/lib/mongo/;
+    mkdir -p ./.mongodb/var/log/mongodb/;
+    mongod --config ./.mongodb/mongo.conf;
+else
+    echo "ℹ️ mongo already running";
+fi
+
+echo "⏳ waiting for mongo service...";
+until mongo --quiet --eval "db.adminCommand('ping')" >/dev/null 2>&1; do
+  sleep 1;
+done
+echo "✅ mongo db now is running";
+
+status=$(mongo --quiet --eval "rs.status().ok" || echo "0");
+
+if [ "$status" != "1" ]; then
+  echo "🔧 initializing replication server set...";
+  mongo --quiet --eval "rs.initiate()";
+  echo "✅ Replication initialized";
+else
+    echo "ℹ️ Replication already initialized";
+fi
