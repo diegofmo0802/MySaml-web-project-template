94f196e07f97b7289b77bf020c373ff6b0e277cd
diegofmo0802 <diegofmo0802@gmail.com>
2026-02-23 04:35:30 +0000

    feat: added commands to package.json for path fixing

--- idx-template.nix
+++ idx-template.nix
@@ -18,10 +18,12 @@
     npm init -y
     npm pkg set type="module"
     npm pkg set main="build/server.js"
-    npm pkg set scripts.compile="npx tsc && npx tsc -p client/logic"
+    npm pkg set scripts.compile="npx tsc && npx tsc -p client/logic && npx dep fix-paths && npx dep fix-paths -p client/logic"
     npm pkg set scripts.build="npm install && npx dep install && npm run compile"
     npm pkg set scripts.dev="npx tsc --watch"
     npm pkg set scripts.dev-client="npx tsc -p client/logic --watch"
+    npm pkg set scripts.fix="npx dep fix-paths --watch"
+    npm pkg set scripts.fix-client="npx dep fix-paths -p client/logic --watch"
     npm pkg set scripts.start="node build/server.js"
     npm install -D typescript @types/node
     npm install saml.servercore${if mode == "dev" then "@dev" else ""}
